84e77ec3b64de8b568eb4afdafcee005bdbb8008
Ram001-code <ramkansal9822@gmail.com>
2025-10-06 00:53:50 +0530

    Feature: added to allowedHosts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['.localhost', 'north-star.ramkansal.com'],
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,
     strictPort: false,
